255dfa09901a61b0cc2d07ff375214633e3fe688
ruzainachougle18 <ruzainachougle18@users.noreply.github.com>
2024-07-21 16:29:30 +0530

    Add signup page

--- login-signup/src/components/Signup.jsx
+++ login-signup/src/components/Signup.jsx
@@ -3,8 +3,16 @@ import "./Signup.css";
 
 const Signup = () => {
     return (
-        <div>
+        <div className="addUser">
             <h3>SignUp</h3>
+            <form className="addUserForm">
+                <div className="input">
+                    <label htmlFor="name">Name</label>
+                    <inout type="text" id="name"></inout>
+                </div>
+            </form>
         </div>
     )
 }
+
+export default Signup;
