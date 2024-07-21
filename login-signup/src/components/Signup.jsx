df32e9dea2b632256cd0d653ed663bd7d43584fd
ruzainachougle18 <ruzainachougle18@users.noreply.github.com>
2024-07-21 17:13:46 +0530

    Added styling

--- login-signup/src/components/Signup.jsx
+++ login-signup/src/components/Signup.jsx
@@ -7,10 +7,22 @@ const Signup = () => {
             <h3>SignUp</h3>
             <form className="addUserForm">
                 <div className="input">
-                    <label htmlFor="name">Name</label>
-                    <inout type="text" id="name"></inout>
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" placeholder="Enter your name" />
+                    <label for="email">Email:</label>
+                    <input type="email" id="mail" placeholder="Enter your Email id" />
+                    <label for="password">Password:</label>
+                    <input type="password" id="p-word" placeholder="Enter password" />
+
+                    <button type="submit" class="create-btn">Sign Up</button>
                 </div>
             </form>
+
+            <div class="login">
+                <p>Already have an account?
+                    <a href="google.com">Log In</a>
+                </p>
+            </div>
         </div>
     )
 }
